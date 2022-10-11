88d85916f1c25144f1b8e68398f52b9a55ff53f6
Gilles de Hollander <gilles.de.hollander@gmail.com>
2022-10-11 10:15:33 +0200

    New run CV on cluster

--- tms_risk/encoding_model/fit_task_cv.py
+++ tms_risk/encoding_model/fit_task_cv.py
@@ -38,8 +38,8 @@ def main(subject, session, bids_folder='/data/ds-tmsrisk', smoothed=False, pca_c
 
     model = GaussianPRF()
     # SET UP GRID
-    mus = np.log(np.linspace(5, 80, 30, dtype=np.float32))
-    sds = np.log(np.linspace(2, 30, 30, dtype=np.float32))
+    mus = np.log(np.linspace(5, 80, 60, dtype=np.float32))
+    sds = np.log(np.linspace(2, 30, 60, dtype=np.float32))
     amplitudes = np.array([1.], dtype=np.float32)
     baselines = np.array([0], dtype=np.float32)
 
