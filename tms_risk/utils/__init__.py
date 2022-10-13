74af2afaaaeec7980f8d1df352d2d655e6830bf6
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2022-10-13 07:50:29 +0000

    get_target_dir

--- tms_risk/utils/__init__.py
+++ tms_risk/utils/__init__.py
@@ -1,1 +1,10 @@
 from .data import *
+
+def get_target_dir(subject, session, sourcedata, base, modality='func'):
+    target_dir = op.join(sourcedata, 'derivatives', base, f'sub-{subject}', f'ses-{session}',
+                         modality)
+
+    if not op.exists(target_dir):
+        os.makedirs(target_dir)
+
+    return target_dir
