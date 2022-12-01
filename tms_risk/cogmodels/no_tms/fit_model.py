02c018bae295ef9c50b7dcd9bfd711b26236ed24
Gilles de Hollander <gilles.de.hollander@gmail.com>
2022-12-01 15:51:01 +0100

    Refactor old Evidencemodels

---
+++ tms_risk/cogmodels/no_tms/fit_model.py
@@ -0,0 +1,67 @@
+import argparse
+from tms_risk.cogmodels.evidence_model import (EvidenceModel,
+                                               EvidenceModelTwoPriors, EvidenceModelGauss, EvidenceModelDiminishingUtility,
+                                               EvidenceModelTwoPriorsDiminishingUtility,
+                                               EvidenceModelDifferentEvidence,
+                                               EvidenceModelDifferentEvidenceTwoPriors, WoodfordModel)
+from tms_risk.utils.data import get_all_behavior
+import os
+import os.path as op
+import arviz as az
+
+
+def main(model_label, burnin=1000, samples=1000, bids_folder='/data/ds-tmsrisk'):
+
+    df = get_all_behavior(bids_folder=bids_folder)
+    df = df.xs(1, 0, 'session')
+    df = df.choice == 2.0
+
+    target_folder = op.join(bids_folder, 'derivatives', 'cogmodels.notms')
+
+    if not op.exists(target_folder):
+        os.makedirs(target_folder)
+
+    target_accept = 0.9
+
+    if model_label == '4':
+        target_accept = 0.95
+    elif model_label == '5':
+        target_accept = 0.9
+    elif model_label == '7':
+        target_accept = 0.95
+
+    model = build_model(model_label, df)
+
+    trace = model.sample(burnin, samples, target_accept=target_accept)
+    az.to_netcdf(trace,
+                 op.join(target_folder, f'model-{model_label}_trace.netcdf'))
+
+def build_model(model_label, df):
+    if model_label == '1':
+        model = EvidenceModel(df)
+    elif model_label == '2':
+        model = EvidenceModelTwoPriors(df)
+    elif model_label == '3':
+        model = EvidenceModelGauss(df)
+    elif model_label == '4':
+        model = EvidenceModelDiminishingUtility(df)
+    elif model_label == '5':
+        model = EvidenceModelTwoPriorsDiminishingUtility(df)
+    elif model_label == '6':
+        model = EvidenceModelDifferentEvidence(df)
+    elif model_label == '7':
+        model = EvidenceModelDifferentEvidenceTwoPriors(df)
+    elif model_label == '8':
+        model = WoodfordModel(df)
+    else:
+        raise Exception(f'Do not know model label {model_label}')
+
+    return model
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_label', default=None)
+    parser.add_argument('--bids_folder', default='/data/ds-tmsrisk')
+    args = parser.parse_args()
+
+    main(args.model_label, bids_folder=args.bids_folder)
---
+++ tms_risk/cogmodels/no_tms/fit_probit.py
@@ -0,0 +1,53 @@
+import os
+import os.path as op
+import arviz as az
+from tms_risk.utils.data import get_all_behavior
+import argparse
+import bambi
+
+
+def main(model_label=1, bids_folder='/data/ds-tmsrisk'):
+
+    model = build_model(model_label, bids_folder)
+
+    idata = model.fit(init='adapt_diag',
+    target_accept=0.9, draws=500, tune=500)
+
+    target_folder = op.join(bids_folder, 'derivatives', 'cogmodels.notms')
+
+    if not op.exists(target_folder):
+        os.makedirs(target_folder)
+
+    az.to_netcdf(idata,
+                 op.join(target_folder, f'model-probit{model_label}_trace.netcdf'))
+
+def build_model(model_label, df, bids_folder='/data/ds-tmsrisk'):
+
+    model_label = int(model_label)
+
+    df = get_all_behavior(bids_folder=bids_folder)
+    df = df.xs(1, 0, 'session')
+
+    df['x'] = df['log(risky/safe)']
+    df['chose_risky'] = df['chose_risky'].astype(bool)
+
+    df = df.reset_index()
+
+
+    if model_label == 1:
+        formula = 'chose_risky ~ x*C(risky_first)*C(n_safe) + (x*C(risky_first)*C(n_safe)|subject)'
+    elif model_label == 2:
+        formula = 'chose_risky ~ x + (x|subject)'
+    elif model_label == 3:
+        formula = 'chose_risky ~ x*C(risky_first) + (x*C(risky_first)|subject)'
+
+    return bambi.Model(formula, data=df, link='probit', family='bernoulli')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_label', default=None)
+    parser.add_argument('--bids_folder', default='/data/ds-tmsrisk')
+    args = parser.parse_args()
+
+    main(args.model_label, bids_folder=args.bids_folder)
