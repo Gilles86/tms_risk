83e58764099f1232e4cb1854a188f231219723dd
Gilles de Hollander <gilles.de.hollander@gmail.com>
2022-12-01 15:48:23 +0100

    Script to quickly do retroicor for all sessions 2/3

---
+++ tms_risk/physiology/session23.m
@@ -0,0 +1,16 @@
+subjects = [3 4 5 6 7 9 10 11 18 19 21 25 26 29 30 31 34 35 36 37 45 46 47 50 53 56 59 62 63 67 69 72 74]
+subjects = [74]
+
+for sub = subjects
+
+    disp(sub);
+    sub_str = sprintf('%02d', sub);
+    disp(sub_str);
+    
+    if (sub ~= 69) && (sub ~= 30)
+        prepare_retroicor(sub_str, '2');
+    end
+
+    prepare_retroicor(sub_str, '3');
+end
+
