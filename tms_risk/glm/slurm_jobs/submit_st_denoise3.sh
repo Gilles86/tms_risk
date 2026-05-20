e1d9570c903b92c06c7b2441a0d7edaa2d06c5cd
Gilles de Hollander <gilles.de.hollander@gmail.com>
2026-05-20 12:21:13 +0200

    glm/slurm_jobs: harden submit_st_denoise{2,3}.sh too (missed in previous commit)

--- tms_risk/glm/slurm_jobs/submit_st_denoise3.sh
+++ tms_risk/glm/slurm_jobs/submit_st_denoise3.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=fit_st_denoise3
-#SBATCH --output=/home/gdehol/logs/fit_st_denoise3_%A-%a.txt
+#SBATCH --account=zne.uzh
+#SBATCH --output=/home/gdehol/logs/fit_st_denoise_ses3_%A-%a.txt
 #SBATCH --ntasks=1
-#SBATCH -c 16
-#SBATCH --time=45:00
+#SBATCH --cpus-per-task=16
+#SBATCH --time=00:45:00
 
-. $HOME/init_conda.sh
+PARTICIPANT_LABEL=$(printf "%02d" "$SLURM_ARRAY_TASK_ID")
+PY="$HOME/data/conda/envs/tms_risk_cpu/bin/python"
+SCRIPT="$HOME/git/tms_risk/tms_risk/glm/fit_single_trials_denoise.py"
+BIDS=/shares/zne.uzh/gdehol/ds-tmsrisk
 
-export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
-
-python $HOME/git/tms_risk/tms_risk/glm/fit_single_trials_denoise.py $PARTICIPANT_LABEL 3 --bids_folder /shares/zne.uzh/gdehol/ds-tmsrisk --smoothed
-python $HOME/git/tms_risk/tms_risk/glm/fit_single_trials_denoise.py $PARTICIPANT_LABEL 3 --bids_folder /shares/zne.uzh/gdehol/ds-tmsrisk
+"$PY" "$SCRIPT" "$PARTICIPANT_LABEL" 3 --bids_folder "$BIDS" --smoothed
+"$PY" "$SCRIPT" "$PARTICIPANT_LABEL" 3 --bids_folder "$BIDS"
